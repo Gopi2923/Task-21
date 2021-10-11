b2a34c88708862607d59447b11f00247b70a3d53
Gopi2923 <85292512+Gopi2923@users.noreply.github.com>
2021-10-11 08:46:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+var h1 = document.getElementById("timer")
+
+const delayedChange = (time, delay, doNext) => {
+    setTimeout(() => {
+        console.log(typeof (time))
+
+        h1.innerHTML = time;
+        doNext && doNext();
+    }, delay)
+}
+
+
+delayedChange('10', 1000, () => {
+    delayedChange('9', 1000, () => {
+        delayedChange('8', 1000, () => {
+            delayedChange('7', 1000, () => {
+                delayedChange('6', 1000, () => {
+                    delayedChange('5', 1000, () => {
+                        delayedChange('4', 1000, () => {
+                            delayedChange('3', 1000, () => {
+                                delayedChange('2', 1000, () => {
+                                    delayedChange('1', 1000, () => {
+                                        delayedChange(' Happy Independence Day ', 1000, () => {
+                                            h1.setAttribute("style", "padding-top:120px;text-align:center")
+                                        })
+                                    })
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    })
+});
